3186284475080c532d9dab33a004dfb0c575e2f0
adlamas <alan12394@hotmail.com>
2026-01-10 18:36:04 -0300

    Add gems jbuilder, pagy. Create route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -11,4 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :people, only: [ :index ]
+    end
+  end
 end
