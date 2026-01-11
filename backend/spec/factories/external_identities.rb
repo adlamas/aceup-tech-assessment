eb1857ef8199053438cc7256cf3a351e4a02531e
adlamas <alan12394@hotmail.com>
2026-01-11 11:06:09 -0300

    Config of gems

--- backend/spec/factories/external_identities.rb
+++ backend/spec/factories/external_identities.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     source { ExternalIdentity::SOURCES.sample }
     
     sequence(:external_id) { |n| "ID-#{n}-#{Faker::Alphanumeric.alphanumeric(number: 5)}" }
-    sequence(:email) { |n| "user#{n}_#{Faker::Internet.unique.email}" }
+    email { Faker::Internet.unique.email }
     
     department { Faker::Job.field }
     external_updated_at { Faker::Time.backward(days: 14) }
